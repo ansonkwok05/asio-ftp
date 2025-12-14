2dffed0f66b588b391a24e2de32cd3a7d89fe48b
ansonkwok05 <kwokanson05@gmail.com>
2025-12-15 06:43:10 +0800

    Remove unplanned/unnescessary stuff

--- src/database/fs_handler.cpp
+++ src/database/fs_handler.cpp
@@ -1,14 +1,9 @@
 #include "fs_handler.h"
-#include "../custom_utils.h"
 
-#include <stdexcept>
 #include <filesystem>
-#include <fstream>
 
 namespace fs_handler
 {
-    using custom_utils::print;
-
     bool directory_exists(std::string path_to_directory)
     {
         return std::filesystem::is_directory(path_to_directory);
@@ -49,46 +44,5 @@ namespace fs_handler
         {
             return false;
         }
-    }
-
-    std::string get_file_description(std::string file_name)
-    {
-        if (!file_exists)
-        {
-            print("File doesn't exists.\n", custom_utils::COLOR::RED);
-            throw std::runtime_error("File path: " + file_name);
-        }
-
-        std::ifstream file(file_name, std::ios::binary);
-
-        std::string file_signature = "";
-        char byte;
-
-        for (int i = 0; i < 15; i++) // 15 as the maximum length the key of FILE_SIGNATURE_TABLE
-        {
-            file.read(&byte, 1);
-            file_signature += HEX_CHARS[(byte & 0xF0) >> 4];
-            file_signature += HEX_CHARS[(byte & 0x0F) >> 0];
-
-            if (FILE_SIGNATURE_DESCRIPTION.find(file_signature) != FILE_SIGNATURE_DESCRIPTION.end())
-                return FILE_SIGNATURE_DESCRIPTION.at(file_signature);
-
-            file_signature += " ";
-        }
-
-        // print("Unknown file signature -> " + file_signature + "from " + file_name + "\n",
-        // custom_utils::COLOR::BLUE);
-        return "";
-    }
-
-    unsigned long long read_file_size(std::string file_name)
-    {
-        if (!file_exists)
-        {
-            print("File doesn't exists.\n", custom_utils::COLOR::RED);
-            throw std::runtime_error("File path: " + file_name);
-        }
-
-        return std::filesystem::file_size(std::filesystem::path(file_name));
     }
 } // namespace fs_handler
--- src/database/fs_handler.h
+++ src/database/fs_handler.h
@@ -1,38 +1,11 @@
 #pragma once
 
 #include <string>
-#include <map>
 
 namespace fs_handler
 {
-    const char HEX_CHARS[16] = {
+    constexpr char HEX_CHARS[16] = {
         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
-    };
-
-    /**
-     * These are from wikipedia. This table suggests the most likely extension of the corresponding file signatures.
-     */
-    const std::map<std::string, std::string> FILE_SIGNATURE_DESCRIPTION = {
-        {"4D 5A", "DOS MZ executable"},
-        {"53 51 4C 69 74 65 20 66 6F 72 6D 61 74 20 33 00", "SQLite Database"},
-        {"7F 45 4C 46", "Executable and Linkable Format"},
-        {"FF D8 FF DB", "JPEG image"},
-        {"FF D8 FF E0 00 10 4A 46 49 46 00 01", "JPEG image"},
-        {"FF D8 FF EE", "JPEG image"},
-        {"FF D8 FF E0", "JPEG image"},
-        {"89 50 4E 47 0D 0A 1A 0A", "PNG image"},
-        {"47 49 46 38 37 61", "GIF image"},
-        {"47 49 46 38 39 61", "GIF image"},
-        {"FF FB", "MP3 audio file"},
-        {"FF F3", "MP3 audio file"},
-        {"FF F2", "MP3 audio file"},
-        {"49 44 33", "MP3 audio file"},
-        {"66 74 79 70 69 73 6F 6D", "MP4 media file"},
-        {"66 74 79 70 4D 53 4E 56", "MP4 media file"},
-        {"7B 5C 72 74 66 31", "Rich Text Format"},
-        {"37 7A BC AF 27 1C", "7-Zip archive"},
-        {"52 61 72 21 1A 07 00", "RAR archive"},
-        {"52 61 72 21 1A 07 01 00", "RAR archive"},
     };
 
     // one-liner filesystem wrapper functions
@@ -40,15 +13,4 @@ namespace fs_handler
     bool file_exists(std::string file_name);
     bool create_directory(std::string path_to_directory);
     bool remove_file(std::string path_to_file);
-
-    /**
-     * Returns the suggested file extension from its file signature
-     * @param file_name File name / Path to file
-     */
-    std::string get_file_description(std::string file_name);
-
-    /**
-     * @param file_name File name / Path to file
-     */
-    unsigned long long read_file_size(std::string file_name);
 } // namespace fs_handler
